98841335674711f84a4538efbfb96954d455f174
tuguldur.j <tuguldur.j@ITZONE-SW-496.itzone.local>
2015-12-26 22:10:47 +0800

    ere

--- src/main/java/com/wbr/pages/About.java
+++ src/main/java/com/wbr/pages/About.java
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on origin/master
 package com.wbr.pages;
 
 import org.apache.tapestry5.annotations.PageActivationContext;
@@ -21,3 +22,27 @@ public class About
 	  System.out.println("test");
   }
 }
+=======
+package com.wbr.pages;
+
+import org.apache.tapestry5.annotations.PageActivationContext;
+
+public class About
+{
+  @PageActivationContext
+  private String learn;
+
+
+  public String getLearn() {
+    return learn;
+  }
+
+  public void setLearn(String learn) {
+    this.learn = learn;
+  }
+  
+  private void test() {
+	  System.out.println("test!!");
+  }
+}
+>>>>>>> e21938b Commit
