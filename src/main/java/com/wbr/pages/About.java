de8b44cecf6e6e91d08b9b12202cded240ba6dca
tuguldur.j <tuguldur.j@ITZONE-SW-496.itzone.local>
2015-12-26 22:36:00 +0800

    Commit;

--- src/main/java/com/wbr/pages/About.java
+++ src/main/java/com/wbr/pages/About.java
@@ -1,28 +1,3 @@
-<<<<<<< Upstream, based on origin/master
-package com.wbr.pages;
-
-import org.apache.tapestry5.annotations.PageActivationContext;
-
-public class About
-{
-  @PageActivationContext
-  private String learn;
-
-
-  public String getLearn() {
-    return learn;
-  }
-
-  public void setLearn(String learn) {
-    this.learn = learn;
-    // comment here
-  }
-  
-  private void test() {
-	  System.out.println("test");
-  }
-}
-=======
 package com.wbr.pages;
 
 import org.apache.tapestry5.annotations.PageActivationContext;
@@ -45,4 +20,3 @@ public class About
 	  System.out.println("test!!");
   }
 }
->>>>>>> e21938b Commit
